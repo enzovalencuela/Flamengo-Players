11aa56e7a17df5a6b419fe5037bd072dbf7ae7b1
Enzo Silva <esilvavalencuela@gmail.com>
2024-09-11 22:14:38 -0400

    Adição de jogadores

--- dados.js
+++ dados.js
@@ -1,6 +1,38 @@
 let dados = [
   //Flamengo------------------------------------------------
 
+  //Agustin Rossi
+  {
+    nome: "Agustin Rossi", // Substitua por seu nome completo se preferir
+    descricao:
+      "Goleiro argentino conhecido por suas defesas difíceis e segurança sob as traves. Um dos destaques do Flamengo.",
+    pagina: "#",
+    imagem:
+      "https://images.flamengo.com.br/public/images/players/1/1710271411.png", // Substitua pela imagem do Rossi
+    tags: "Argentina, Flamengo, Agustín Rossi, goleiro, todos",
+    numero: "1",
+    posicao: "Goleiro",
+    titulos: "Campeonato Carioca (2023) <br> Libertadores (2022)", // Adicione os títulos conquistados pelo Rossi
+    dataNascimento: "09/01/1995", // Verifique a data de nascimento correta
+    curiosidades:
+      "Chegou ao Flamengo em 2022 e logo se tornou ídolo da torcida. Conhecido por sua liderança e habilidade em cobranças de pênalti.",
+  },
+  //Alex Sandro
+  {
+    nome: "Alex Sandro",
+    descricao:
+      "Lateral-esquerdo brasileiro com vasta experiência no futebol europeu. Chegou ao Flamengo em 2024 e traz consigo muita experiência e qualidade.",
+    pagina: "https://www.instagram.com/alxsndro12/", // Atualize com o link correto
+    imagem:
+      "https://images.flamengo.com.br/public/images/players/4/1726081099.jpg", // Substitua pela imagem do Alex Sandro
+    tags: "Brasil, Flamengo, Alex Sandro, lateral-esquerdo, experiência, Europa, todos",
+    numero: 13,
+    posicao: "Lateral-esquerdo",
+    titulos: "Série A Italiana (vários títulos) <br> Copa América (2019)", // Adicione os títulos conquistados pelo Alex Sandro
+    dataNascimento: "26/01/1991",
+    curiosidades:
+      "Com passagens por Juventus e Seleção Brasileira, Alex Sandro é um dos laterais mais experientes do futebol mundial.",
+  },
   // Ayrton Lucas Ferreira da Silva
   {
     nome: "Ayrton Lucas",
@@ -34,6 +66,38 @@ let dados = [
     dataNascimento: "30/12/1990",
     curiosidades:
       "Chegou ao Flamengo em 2019 e rapidamente se tornou ídolo da torcida.",
+  },
+  // Carlinhos
+  {
+    nome: "Carlinhos",
+    descricao:
+      "Atacante versátil, capaz de atuar por diversas posições no ataque. Conhecido por sua força física e finalizações de média distância.",
+    pagina: "#", // Atualize com o link correto
+    imagem:
+      "https://images.flamengo.com.br/public/images/players/7/1725043943.png",
+    tags: "Brasil, Flamengo, Carlinhos, atacante, versátil, força física, todos",
+    numero: "22",
+    posicao: "Atacante",
+    titulos: "Campeonato Paulista (2022)", // Adicione os títulos conquistados pelo Carlinhos
+    dataNascimento: "18/09/1994",
+    curiosidades:
+      "Chegou ao Flamengo em 2024 e rapidamente se tornou uma peça importante no esquema tático do time.",
+  },
+  // Carlos Alcaraz
+  {
+    nome: "Carlos Alcaraz",
+    descricao:
+      "Meia argentino com grande técnica e visão de jogo. Chegou ao Flamengo em 2024 e rapidamente se adaptou ao estilo de jogo do time.",
+    pagina: "https://www.instagram.com/_alcaraz22/",
+    imagem:
+      "https://images.flamengo.com.br/public/images/players/6/1726080833.jpg",
+    tags: "Argentina, Flamengo, Alcaraz, meia, técnica, visão de jogo, todos",
+    numero: "8",
+    posicao: "Meia",
+    titulos: "Campeonato Argentino (2022)", // Adicione os títulos conquistados pelo Alcaraz
+    dataNascimento: "22/07/1998",
+    curiosidades:
+      "Conhecido por sua habilidade em passes longos e visão de jogo, Alcaraz é um dos principais armadores do Flamengo.",
   },
   // David Luiz Moreira Marinho
   {
@@ -84,6 +148,22 @@ let dados = [
     dataNascimento: "22/07/1996",
     curiosidades:
       "Teve destaque no Grêmio e conquistou a Copa América de 2019 com a Seleção Brasileira.",
+  },
+  //Fabrício Bruno
+  {
+    nome: "Fabrício Bruno",
+    descricao:
+      "Zagueiro brasileiro conhecido por sua força física e liderança dentro de campo. Um dos pilares da defesa do Flamengo.",
+    pagina: "https://www.instagram.com/fabriciobruno96/",
+    imagem:
+      "https://images.flamengo.com.br/public/images/players/2/1710271630.png", // Substitua pela imagem do Fabrício Bruno
+    tags: "Brasil, Flamengo, Fabrício Bruno, zagueiro, força física, liderança, todos",
+    numero: "3",
+    posicao: "Zagueiro",
+    titulos: "Campeonato Carioca (2023) <br> Libertadores (2022)", // Adicione os títulos conquistados pelo Fabrício Bruno
+    dataNascimento: "22/09/1996", // Verifique a data de nascimento correta
+    curiosidades:
+      "Revelado pelo Botafogo, chegou ao Flamengo em 2021 e rapidamente se tornou titular absoluto.",
   },
   // Gabriel Barbosa
   {
@@ -152,6 +232,22 @@ let dados = [
     curiosidades:
       "Chegou ao Flamengo em 2019 e rapidamente se tornou um dos principais jogadores do time.",
   },
+  //Léo Ortiz
+  {
+    nome: "Léo Ortiz",
+    descricao:
+      "Zagueiro brasileiro conhecido por sua técnica e visão de jogo. Chegou ao Flamengo em 2024 e já se destaca pela sua qualidade.",
+    pagina: "https://www.instagram.com/leortiz33/",
+    imagem:
+      "https://images.flamengo.com.br/public/images/players/9/1710431451.png",
+    tags: "Brasil, Flamengo, Léo Ortiz, zagueiro, técnica, visão de jogo, todos",
+    numero: "4",
+    posicao: "Zagueiro",
+    titulos: "Sem títulos pelo Flamengo", // Adicione os títulos conquistados pelo Léo Ortiz no Flamengo (se houver)
+    dataNascimento: "11/01/1997", // Verifique a data de nascimento correta
+    curiosidades:
+      "Antes do Flamengo, se destacou no Red Bull Bragantino, formando uma dupla de zaga sólida com Fabrício Bruno.",
+  },
   // Léo Pereira
   {
     nome: "Leonardo Pereira",
@@ -168,6 +264,70 @@ let dados = [
     dataNascimento: "05/01/1998",
     curiosidades:
       "Revelado pelo Athletico Paranaense, chegou ao Flamengo em 2019 e rapidamente se consolidou como titular.",
+  },
+  // Luiz Araújo
+  {
+    nome: "Luiz Araújo",
+    descricao:
+      "Atacante rápido e habilidoso, conhecido por seus dribles e finalizações precisas. Uma das principais armas do ataque do Flamengo.",
+    pagina: "#", // Atualize com o link correto
+    imagem:
+      "https://images.flamengo.com.br/public/images/players/7/1710273652.png",
+    tags: "Brasil, Flamengo, Luiz Araújo, atacante, habilidoso, rápido, todos",
+    numero: "7",
+    posicao: "Atacante",
+    titulos: "Campeonato Brasileiro (2023)", // Adicione os títulos conquistados pelo Luiz Araújo
+    dataNascimento: "02/06/1996",
+    curiosidades:
+      "Com passagens por clubes europeus, Luiz Araújo traz experiência internacional para o Flamengo.",
+  },
+  //Matías Viña
+  {
+    nome: "Matías Viña",
+    descricao:
+      "Lateral-esquerdo uruguaio conhecido por sua habilidade nos cruzamentos e marcação. Chegou ao Flamengo em 2021 e rapidamente se tornou titular.",
+    pagina: "https://www.instagram.com/matiasv17/",
+    imagem:
+      "https://images.flamengo.com.br/public/images/players/4/1710272288.png",
+    tags: "Uruguai, Flamengo, Matías Viña, lateral-esquerdo, cruzamentos, marcação, todos",
+    numero: "22",
+    posicao: "Lateral-esquerdo",
+    titulos: "Campeonato Carioca (2021) <br> Supercopa do Brasil (2021)", // Adicione os títulos conquistados pelo Viña
+    dataNascimento: "09/11/1997", // Verifique a data de nascimento correta
+    curiosidades:
+      "Revelado pelo Nacional do Uruguai, Viña já defendeu a seleção uruguaia em diversas oportunidades.",
+  },
+  //Matheus Cunha
+  {
+    nome: "Matheus Cunha",
+    descricao:
+      "Goleiro brasileiro, jovem promessa das categorias de base do Flamengo. Destaca-se por suas reflexões e agilidade.",
+    pagina: "https://www.instagram.com/matheuscunha_01/",
+    imagem:
+      "https://images.flamengo.com.br/public/images/players/1/1710271566.png",
+    tags: "Brasil, Flamengo, Matheus Cunha, goleiro, base, todos",
+    numero: "30",
+    posicao: "Goleiro",
+    titulos: "Campeonato Carioca Sub-20 (2023)", // Adicione os títulos conquistados pelo Matheus Cunha
+    dataNascimento: "15/03/2004", // Verifique a data de nascimento correta
+    curiosidades:
+      "Considerado uma das maiores promessas do futebol brasileiro na posição. Já defendeu a Seleção Brasileira de base.",
+  },
+  // Matheus Gonçalves
+  {
+    nome: "Matheus Gonçalves",
+    descricao:
+      "Meia brasileiro com grande potencial, conhecido por sua habilidade nos dribles e visão de jogo. Uma das maiores promessas das categorias de base do Flamengo.",
+    pagina: "https://www.instagram.com/oficialm10_/",
+    imagem:
+      "https://images.flamengo.com.br/public/images/players/6/1710273017.png", // Substitua pela imagem do Matheus Gonçalves
+    tags: "Brasil, Flamengo, Matheus Gonçalves, meia, base, jovem, todos",
+    numero: "10",
+    posicao: "Meia",
+    titulos: "Campeonato Brasileiro Sub-20 (2023)", // Adicione os títulos conquistados pelo Matheus Gonçalves
+    dataNascimento: "15/03/2004",
+    curiosidades:
+      "Revelado nas categorias de base do Flamengo, Matheus Gonçalves já é considerado um dos principais talentos do futebol brasileiro.",
   },
   // Nicolás De La Cruz
   {
@@ -201,5 +361,21 @@ let dados = [
       "Campeonato Carioca (2020, 2021, 2023, 2024) <br> Brasileirão (2020) <br> Libertadores (2022) <br> Copa do Brasil (2022) <br> Supercopa do Brasil (2020, 2021)",
     dataNascimento: "20/06/1997",
     curiosidades: "Revelado pelo Fluminense, ídolo da torcida rubro-negra.",
+  },
+  //Wesley
+  {
+    nome: "Wesley",
+    descricao:
+      "Meio-campista brasileiro conhecido por sua versatilidade e habilidade nos passes. Destaca-se por sua rápida adaptação ao time titular do Flamengo.",
+    pagina: "https://www.instagram.com/wesleyfranca03_/?hl=pt",
+    imagem:
+      "https://images.flamengo.com.br/public/images/players/3/1710272030.png",
+    tags: "Brasil, Flamengo, Wesley, meio-campista, versatilidade, passes, todos",
+    numero: "43",
+    posicao: "Meio-campista",
+    titulos: "Copa do Brasil (2022)", // Adicione os títulos conquistados pelo Wesley
+    dataNascimento: "06/09/2003",
+    curiosidades:
+      "Revelado nas categorias de base do Flamengo, Wesley tem se mostrado uma das grandes promessas do futebol brasileiro.",
   },
 ];
